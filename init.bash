111160480d5fb005ca33707220f3138740364e35
Travis Austin <travis.austin@rezitech.com>
2017-06-08 20:52:49 +0000

    Can now include domain name when running init.bash

--- init.bash
+++ init.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Execute this from the commandline like this:
-# bash <(curl -s https://raw.githubusercontent.com/rezitech/letsencrypt-unifi-nvr/master/init.bash)
+# bash <(curl -s https://raw.githubusercontent.com/rezitech/letsencrypt-unifi-nvr/master/init.bash) DOMAIN-NAME-HERE.COM
 # This script will install the package in one step
 
 apt-get install -y git
 git clone https://github.com/rezitech/letsencrypt-unifi-nvr.git /usr/local/bin/letsencrypt-unifi-nvr/
 
-echo $1
+/usr/local/bin/letsencrypt-unifi-nvr/letsencrypt-unifi-nvr.bash -i -d $1
