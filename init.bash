d465faf479e6f89f369b0064f1b341e5d31ff822
Travis Austin <travis.austin@rezitech.com>
2017-06-08 20:40:04 +0000

    Added init script to allow one-line install of the main script

---
+++ init.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Execute this from the commandline like this:
+# bash <(curl -s https://raw.githubusercontent.com/rezitech/letsencrypt-unifi-nvr/master/init.bash)
+# This script will install the package in one step
+
+apt-get install -y git
+git clone https://github.com/rezitech/letsencrypt-unifi-nvr.git /usr/local/bin/
